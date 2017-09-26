9cd373beeee7b9ed8aa751f8e3d7e852f13cb08c
Anshul Ghogre <30301360+aghogre@users.noreply.github.com>
2017-09-26 16:21:01 +0530

    Delete kafka-docker.sh

--- kafka-docker.sh
+++
@@ -1,8 +0,0 @@
-docker run -d -p 2181:2181 --name zookeeper jplock/zookeeper
-
-docker run -d --name kafka --link zookeeper:zookeeper ches/kafka
-
-docker ZK_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' zookeeper)
-docker KAFKA_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' kafka)
-
-docker run --rm ches/kafka
